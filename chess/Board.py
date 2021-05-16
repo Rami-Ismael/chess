c55accf74a3a0e22fbf46fb81e899a7a402ff617
Rami-Ismael <rami.m.ismael@gmail.com>
2021-05-16 10:26:47 -0500

    add all the white piece in on the bottom

--- chess/Board.py
+++ chess/Board.py
@@ -20,13 +20,13 @@ class Board:
         for row in range(ROWS):
             self.board.append([])
             for col in range( COLS):
-               if row==1 :
+               if row==6 :
                     self.board[row].append(Pond(WHITE_SECTION,row,col))
-               elif row==0 and (col==0 or col==7):
+               elif row==7 and (col==0 or col==7):
                     self.board[row].append(Rook(WHITE_SECTION,row,col))
-               elif row==0 and (col==1 or col==6):
+               elif row==7 and (col==1 or col==6):
                    self.board[row].append(Knight(WHITE_SECTION,row,col))
-               elif row==0 and (col==2 or col==5):
+               elif row==7 and (col==2 or col==5):
                    self.board[row].append(Bishop(WHITE_SECTION,row,col))
                else:
                    self.board[row].append(0)
