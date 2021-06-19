6d6440a4e3665c75a9e0cc2c5fbfe39ba0bfc483
Rami-Ismael <rami.m.ismael@gmail.com>
2021-06-19 13:59:34 -0500

    add the pond piece movement

--- chess/piece/Pond.py
+++ chess/piece/Pond.py
@@ -6,6 +6,7 @@ class Pond(Piece):
     def __inti__(self,color,row_position,column_position):
         super().__init__(color,row_position,column_position)
         self.pond_moves=[(1,0)]
+        self.first_turn = True
         self.relative_position
         #white will alway be on the bottom 
         if color=="white":
@@ -14,7 +15,10 @@ class Pond(Piece):
             self.relative_position = -1
         self.calc_poss()
     def movement(self):
-        return self.pond_moves[0]
+        move = []
+        if(self.first_turn):
+            move.append([(self.col,self.row+1),(self.col,self.row+2)])
+        return move.append([(self.col,self.row+1)
     def draw(self,screen):
         if self.color =="white":
             screen.blit(WHITE_PAWN,(self.x,self.y)) 
